845cedb0bf6420a56b4ccb5448fd3e13a5bd8bba
abhisheksharma <abhisheksharma131098@gmail.com>
2022-05-31 13:37:10 +0530

    replaced useState with userReducer on the show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,14 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+
+const reducer = (prevState, action) => {
+  switch(action.type){
+    case 'FETCH_SUCCESS':
+      return {isLoading: false, show: action.show, error: null};
+    case 'FETCH_FAILED':
+      return {...prevState, isLoading: false, error: action.error}
+    default:
+      return prevState;
+  }
+}
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+} 
 
 const Show = () => {
 
   const { id } = useParams();
 
-  const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState); 
+  console.log(state);
+  // const [show, setShow] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
 
   useEffect(() => {
 
@@ -17,13 +36,11 @@ const Show = () => {
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(results => {
         if (isMounted) {
-          setShow(results);
-          setIsLoading(false);
+          dispatch({type: 'FETCH_SUCCESS', show: results});
         }
       }).catch(err => {
         if (isMounted) {
-          setError(err.message);
-          setIsLoading(false);
+          dispatch({type: 'FETCH_FAILED', error: err.message})
         }
       });
     return () => {
@@ -31,14 +48,14 @@ const Show = () => {
     }
   }, [id]);
 
-  console.log('show', show);
+  console.log('show', state.show);
 
-  if (isLoading) {
+  if (state.isLoading) {
     return <div>Data is being loaded</div>
   }
 
-  if (error) {
-    return <div>Error occured: {error}</div>
+  if (state.error) {
+    return <div>Error occured: {state.error}</div>
   }
 
   return (
