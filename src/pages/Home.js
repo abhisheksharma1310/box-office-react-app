604a1c1b9e7c013aaf83b4a4532f1a36054baf39
abhisheksharma <abhisheksharma131098@gmail.com>
2022-05-31 18:34:05 +0530

    defined theme for styled components and created a few styled components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,6 @@ const Home = () => {
     const onSearch = () => { 
         apiGet(`/search/${searchOption}?q=${input}`).then(result => {
             setResults(result);
-            console.log(result);
         });
     };
 
